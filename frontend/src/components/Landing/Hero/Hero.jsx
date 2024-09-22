d0c952a1d46abfc6bfefbf3c20f35b245b27fbf3
Penguin <ishanjaiswal370@gmail.com>
2024-09-23 03:20:47 +0530

    clean up

---
+++ frontend/src/components/Landing/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import Content from "../Content/Content";
+import landing from "../../../assets/landing.png";
+
+const Hero = () => {
+	return (
+		<div className="flex items-center justify-center gap-20 bg-background py-28">
+			<Content />
+			<img
+				src={landing}
+				alt="Landing"
+				className="drop-shadow-4xl h-96 rounded-xl"
+			/>
+		</div>
+	);
+};
+
+export default Hero;
--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -1,17 +1,7 @@
-import Content from "./Content/Content";
-import landing from "../../assets/landing.png";
+import Hero from "./Hero/Hero";
 
 const Landing = () => {
-	return (
-		<div className="flex items-center justify-center gap-20 bg-background py-28">
-			<Content />
-			<img
-				src={landing}
-				alt="Landing"
-				className="drop-shadow-4xl h-96 rounded-xl"
-			/>
-		</div>
-	);
+	return <Hero />;
 };
 
 export default Landing;
